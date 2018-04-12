e818ab2575e8ac054068ac7fa503da0a1cfa93aa
Dillon Merritt <merrittd42@gmail.com>
2018-04-12 18:56:08 -0400

    Added scraping code for Twitter.

---
+++ scrape.py
@@ -0,0 +1,33 @@
+import twitter
+from time import gmtime, strftime
+
+api = twitter.Api(
+ consumer_key='E50y90pR3tyRcJM2VrC2fA0CR',
+ consumer_secret='EVGldnE7MlLlQi13OsYnL7gyIUxUj5vprnzaKyM4YKm5huEaOY',
+ access_token_key='2177399604-3CnyxZqPCGlCyvzxUSsjXqseX0JFhpishSV0ioV',
+ access_token_secret='jbA28AZwfCieK8CVJG1wIjWxE4djNwkOvrKTqKiAkKBiN',
+ sleep_on_rate_limit=True)
+
+results = api.GetSearch(term="#ReadyPlayerOne", raw_query=None, geocode=None, since_id=None, max_id=980694526259093504, count=100, lang="en", locale=None, result_type='recent', include_entities=False)
+f= open("tweetListFinalShotForever22.txt","w+")
+tweetCount = 0
+id = results[0].id
+print('Lowest ID is: ' + str(id))
+while True:
+    for result in results:
+
+        if(id > result.id):
+            id = result.id
+            print('Lowest ID is: ' + str(id))
+        
+        f.write('[' + result.created_at + "]," + result.text.replace('\n',' ').replace(',',' '))
+        f.write("\n")
+        tweetCount = tweetCount + 1
+    f.flush()
+
+    results = api.GetSearch(term="#ReadyPlayerOne", raw_query=None, geocode=None, max_id=id, count=100, lang="en", locale=None, result_type='recent', include_entities=False)
+    print('Grabbing more tweets...')
+
+
+
+    
